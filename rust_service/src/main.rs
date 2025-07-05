f7314eb1fda22e3f7199358c7aaa67e1fd2d5de7
innoky <romanovinno@gmail.com>
2025-07-05 20:24:31 +0300

    /start comment bug fix

--- rust_service/src/main.rs
+++ rust_service/src/main.rs
@@ -1,12 +1,33 @@
-fn test(s: &String) -> String
-{
-    s = String::from("hell");
-    s
+use actix_web::{web, App, HttpResponse, HttpRequest, HttpServer, Responder};
+use actix_web::body::BoxBody;
+
+struct MyResponse {
+    message: String,
 }
 
-fn main()
-{
-    let s = String::from("hello");
-    let b = test(&s);
-    println!("{}",b);
+impl Responder for MyResponse {
+    type Body = BoxBody;  // <-- обязательно указываем связанный тип
+
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        HttpResponse::Ok()
+            .content_type("vmlx/data; charset=utf-8")
+            .body(self.message)
+    }
+}
+async fn index() -> impl Responder {
+    MyResponse {
+        message: "Hello from MyResponse!".to_string(),
+    }
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::post().to(index)) // Регистрируем маршрут
+    })
+    .bind("127.0.0.1:8080")? // Слушаем порт 8080
+    .run()
+    .await
 }
